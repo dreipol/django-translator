4f001763c34b255d45f61bf51419eb891ab44153
Christian Schürmann <christian.schuermann@bluewin.ch>
2018-04-20 17:01:49 +0200

    Release 1.0.11

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-translator',
-    version='1.0.10',
+    version='1.0.11',
     packages=['translator'],
     include_package_data=True,
     license='MIT License',
