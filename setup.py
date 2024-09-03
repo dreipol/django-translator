6e5cc999e4364a60541a4ef45ac02be571a60527
Christian Schürmann <christian.schuermann@bluewin.ch>
2024-09-03 16:19:37 +0200

    Release 1.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-translator',
-    version='1.1.1',
+    version='1.2.0',
     packages=['translator'],
     include_package_data=True,
     license='MIT License',
